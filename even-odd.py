a5a802e03ba338f0ca1abc69a4dbbcd24b1e50b8
Ganesh-Ingaladal <ganeshingaladal1@gmail.com>
2026-09-11 16:15:50 +0530

    Update even-odd.py

--- even-odd.py
+++ even-odd.py
@@ -2,7 +2,7 @@ num = int(input("Enter a number: "))
 
 if num % 2 == 0:
     print("The number is Even")
-    ..
+    print("it is Even")
 else:
     print("The number is Odd")
-    ..
+    print("it is odd")
